ded3f62693db0bfc1e11af985d19963c980dc734
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2022-07-07 13:40:58 +0530

    add rent payment dto ,entity

--- src/main/java/lk/ijse/spring/dto/RentPaymentDTO.java
+++ src/main/java/lk/ijse/spring/dto/RentPaymentDTO.java
@@ -1,9 +1,12 @@
 package lk.ijse.spring.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,10 +15,18 @@ import lombok.ToString;
 public class RentPaymentDTO {
 
     private String paymentId;
-    private String paymentId;
-    private String paymentId;
-    private String paymentId;
-    private String paymentId;
-    private String paymentId;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate paymentDate;
+    private double rentFee;
+    private String harmOrNot;
+    private double loseDamagePayment;
+    private double reduceLoseDamagePayment;
+    private double driverFee;
+    private double travelledDistance;
+    private double extraKm;
+    private double extraKmPrice;
+    private double fullPayment;
+
+    private ReserveDetailsDTO reserveDetails;
 
 }
--- src/main/java/lk/ijse/spring/entity/RentPayment.java
+++ src/main/java/lk/ijse/spring/entity/RentPayment.java
@@ -1,4 +1,38 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class RentPayment {
+
+    @Id
+    private String paymentId;
+
+    private LocalDate paymentDate;
+    private double rentFee;
+    private String harmOrNot;
+    private double loseDamagePayment;
+    private double reduceLoseDamagePayment;
+    private double driverFee;
+    private double travelledDistance;
+    private double extraKm;
+    private double extraKmPrice;
+    private double fullPayment;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private ReserveDetails reserveDetails;
+
 }
